806aaf601c6d9fec1f6fdf2bc115c2677d33ce56
Reo-Koda <matoryoshika719@gmail.com>
2025-04-20 22:55:47 +0900

    deploy weather api

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
     images: {
       domains: ['openweathermap.org'],
     },
+    env: {
+      NEXT_PUBLIC_OPEN_WEATHER_MAP_KEY: process.env.NEXT_PUBLIC_OPEN_WEATHER_MAP_KEY,
+    },
 };
 
 export default nextConfig;
